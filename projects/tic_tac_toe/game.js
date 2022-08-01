8685b5f89b12ae76552f1fde8915f6ef75d64abb
Navleen <navleensandhu0211@gmail.com>
2022-08-01 16:45:06 -0700

    worked on UI for tic tac toe game  - done with html and css code

---
+++ projects/tic_tac_toe/game.js
@@ -0,0 +1,17 @@
+const box11_div = document.querySelector("#box11");
+
+
+const updateUI = () =>{
+  box11_div.innerText = game.restart();
+}
+
+class game{
+  constructor(){
+
+  }
+
+  restart(){
+
+  }
+}
+
