109ec6f59c0493ff6f527c51687d595370d65d75
Navleen <navleensandhu0211@gmail.com>
2022-08-02 08:16:04 -0700

    updated the innerText of each box based on the current state

--- projects/tic_tac_toe/game.js
+++ projects/tic_tac_toe/game.js
@@ -8,14 +8,17 @@ const vals = [
   ['', '', ''],
 ]
 
-const updateUI = () =>{
-  //box00_div.innerText = vals[0][0];
+const updateUI = () => {
+  for(let i = 0; i < vals.length; i++){
+    for(let j = 0; j < vals[i].length; j++){
+      document.querySelector(`#box${i}${j}`).innerText =  vals[i][j];
+    }
+  }
 
 }
 
 const handleBoxClick = (row,column) => {
-  console.log(row, column);
-  vals[row][column] = 'X'
+  vals[row][column] = 'X';
 
   updateUI();
 }
