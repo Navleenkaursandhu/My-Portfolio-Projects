191da4de2306dc8aab5c20b9e093190367719dd2
Navleen <navleensandhu0211@gmail.com>
2022-08-02 10:30:03 -0700

    implemented restart functionality and formatted the code

--- projects/tic_tac_toe/game.js
+++ projects/tic_tac_toe/game.js
@@ -1,10 +1,8 @@
-
-
 const restartDiv = document.querySelector("#restart-button");
 const playerNameDiv = document.querySelector("#player-name");
 const winnerDiv = document.querySelector("#winner-name");
 
-const vals = [
+let vals = [
   ['', '', ''],
   ['', '', ''],
   ['', '', ''],
@@ -19,7 +17,7 @@ const updateUI = () => {
       document.querySelector(`#box${i}${j}`).innerText = vals[i][j];
     }
   }
-
+  
   playerNameDiv.innerText = currentPlayer;
   winnerDiv.innerText = winner;
 }
@@ -32,17 +30,16 @@ const handleBoxClick = (row, column) => {
     if (vals[row][0] === vals[row][1] && vals[row][1] === vals[row][2]) {
       winner = currentPlayer;
     }
-    else if ((vals[0][column] === vals[1][column] && vals[1][column] === vals[2][column])) {
+    else if (vals[0][column] === vals[1][column] && vals[1][column] === vals[2][column]) {
       winner = currentPlayer;
     }
-    else if ((vals[1][1] && vals[0][0] === vals[1][1] && vals[1][1] === vals[2][2])){
+    else if (vals[1][1] && vals[0][0] === vals[1][1] && vals[1][1] === vals[2][2]){
       winner = currentPlayer;
     }
-    else if((vals[1][1] && vals[0][2] === vals[1][1] && vals[1][1] === vals[2][0])){
+    else if(vals[1][1] && vals[0][2] === vals[1][1] && vals[1][1] === vals[2][0]){
       winner = currentPlayer;
     }
-
-
+  
     if (currentPlayer === "X") {
       currentPlayer = "O";
     }
@@ -50,14 +47,19 @@ const handleBoxClick = (row, column) => {
       currentPlayer = "X";
     }
   }
-
   updateUI();
 }
 
-
-
-
-
+const restart = restartDiv.onclick = () => {
+  currentPlayer = "X";
+  winner = "";
+  vals = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]
+updateUI();
+}
 
 for (let i = 0; i < vals.length; i++) {
   for (let j = 0; j < vals[i].length; j++) {
