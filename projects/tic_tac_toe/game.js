787bfe1e2997a160e43518a984373bcb1aabaa79
Navleen <navleensandhu0211@gmail.com>
2022-08-02 08:01:45 -0700

    added onclick listener on all the boxes

--- projects/tic_tac_toe/game.js
+++ projects/tic_tac_toe/game.js
@@ -1,17 +1,32 @@
-const box11_div = document.querySelector("#box11");
 
+
+const restart_div = document.querySelector("#restart-button");
+
+const vals = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+]
 
 const updateUI = () =>{
-  box11_div.innerText = game.restart();
+  //box00_div.innerText = vals[0][0];
+
 }
 
-class game{
-  constructor(){
+const handleBoxClick = (row,column) => {
+  console.log(row, column);
+  vals[row][column] = 'X'
 
-  }
-
-  restart(){
-
-  }
+  updateUI();
 }
 
+
+
+
+
+
+for(let i = 0; i < vals.length; i++){
+  for(let j = 0; j < vals[i].length; j++){
+    document.querySelector(`#box${i}${j}`).onclick = () => handleBoxClick(i,j);
+  }
+}
