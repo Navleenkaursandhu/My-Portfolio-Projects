0bf74b6dd568cef5be66a4f7d6d6e8603667ea63
Navleen <navleensandhu0211@gmail.com>
2022-08-03 21:12:49 -0700

    implemented the functionality of TODO list web page

--- projects/TODOList/list.js
+++ projects/TODOList/list.js
@@ -1,13 +1,32 @@
-const addTaskDiv= document.querySelector("#add-task");
-const inputTaskDiv= document.querySelector("#input-task");
-
-
-
+const addTaskDiv = document.querySelector("#add-task");
+const inputTask = document.querySelector("#input-task");
+const unorderedList = document.querySelector("#list");
 
 addTaskDiv.onclick = () => {
-  
-  console.log(inputTaskDiv.value);
+  let text = inputTask.value;
   inputTaskDiv.value = "";
 
-  
+  addList(text);
 }
+
+const addList = (text) => {
+  let listElement = document.createElement("li");
+  listElement.innerText = text;
+
+  let doneButton = document.createElement("button");
+  doneButton.style.marginLeft = "10px";
+  doneButton.innerText = "DONE";
+
+  unorderedList.appendChild(listElement);
+  listElement.appendChild(doneButton);
+
+  taskDone(doneButton, listElement);
+}
+
+const taskDone = (doneBtn, listElem) => {
+  doneBtn.onclick = () => {
+    console.log("done task");
+    listElem.remove();
+  }
+}
+
