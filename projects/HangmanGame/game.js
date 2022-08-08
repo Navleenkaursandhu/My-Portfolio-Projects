4d0b4f3be70a6c8c14f4953c07215cb93ef0252f
Navleen <navleensandhu0211@gmail.com>
2022-08-08 13:27:18 -0700

    implemented user win case logic and did code formatting

--- projects/HangmanGame/game.js
+++ projects/HangmanGame/game.js
@@ -2,9 +2,12 @@ const playDiv = document.querySelector("#play");
 const wordDiv = document.querySelector("#word");
 const resultDiv = document.querySelector("#result");
 let eventHandlerArray = [];
+let isPlayerPlaying = true;
 
 let circleCount = 1;
+
 playDiv.onclick = async () => {
+  isPlayerPlaying = true;
   const resultedWord = await fetch("https://puzzle.mead.io/puzzle?wordCount=1")
     .then(response => response.json())
     .then(response => response.puzzle)
@@ -36,35 +39,38 @@ playDiv.onclick = async () => {
     wordDiv.appendChild(charDiv);
 
     const eventHandler = (e) => {
-      console.log(resultedWord, i);
-      const capitalChar = e.key.toUpperCase();
-      if (capitalChar === charArray[i]) {
-        charDiv.innerText = capitalChar;
-      }
+      if (circleCount <= 16 && isPlayerPlaying) {
+        const capitalChar = e.key.toUpperCase();
+        if (capitalChar === charArray[i]) {
+          charDiv.innerText = capitalChar;
 
-      if (i === 0) {
-        if (!charArray.includes(capitalChar) && circleCount <= 16) {
-          document.querySelector(`#circle${circleCount}`).innerText = capitalChar;
-          document.querySelector(`#circle${circleCount}`).classList.add("text-center", "background-red");
-
-          if (!charArray.includes(capitalChar) && circleCount === 16) {
-            resultDiv.innerText = "you lost";
+          let playerHasWon = true;
+          let arrayOfCharElem = document.querySelectorAll("#word > div ");
+          for (let i = 0; i < charArray.length; i++) {
+            if (charArray[i] !== arrayOfCharElem[i].innerText) {
+              playerHasWon = false;
+            }
           }
-          circleCount++;
+          if (playerHasWon) {
+            resultDiv.innerText = "You Won:)";
+            isPlayerPlaying = false;
+          }
         }
-        // if(circleCount !== 16){
-        //   for(let i = 0; i < charArray.length; i++ ){
-        //     if(charArray[i] === document.querySelector("#word:nth-child(i)").innerText);
 
-        //   }
-        //resultDiv.innerText = "you won";
+        if (i === 0) {
+          if (!charArray.includes(capitalChar) && circleCount <= 16) {
+            document.querySelector(`#circle${circleCount}`).innerText = capitalChar;
+            document.querySelector(`#circle${circleCount}`).classList.add("text-center", "background-red");
+
+            if (!charArray.includes(capitalChar) && circleCount === 16) {
+              resultDiv.innerText = "You Lost:(";
+              isPlayerPlaying = false;
+            }
+            circleCount++;
+          }
+
+        }
       }
-      // else if(!charArray.includes(capitalChar) && document.querySelector("#circle15").innerText === capitalChar){
-      //   resultDiv.innerText = "you lost";
-      // }
-      // else {
-      //   resultDiv.innerText = "you won";
-      // }
     }
     document.addEventListener("keydown", eventHandler);
 
