538b379b0462d619ca42aebc38869dbe5bb2b956
Navleen <navleensandhu0211@gmail.com>
2022-08-08 09:21:29 -0700

    implemented logic if user input does not match any character
     -  used  removeEventListener to remove the previous history of events

--- projects/HangmanGame/game.js
+++ projects/HangmanGame/game.js
@@ -1,32 +1,9 @@
 const playDiv = document.querySelector("#play");
 const wordDiv = document.querySelector("#word");
 
-// playDiv.onclick = async () => {
-//   const resultedWord = await fetch("https://puzzle.mead.io/puzzle?wordCount=1")
-//     .then(response => response.json())
-//     .then(response => response.puzzle)
+let eventHandlerArray = [];
 
-//     wordDiv.innerHTML = "";
-
-//   const charArray = resultedWord.toUpperCase().split("");
-
-//   for (let i = 0; i < charArray.length; i++) {
-//     let charDiv = document.createElement("div");
-//     console.log(charDiv);
-
-//     charDiv.innerText = charArray[i];
-//     //charDiv.innerText = "";
-//     charDiv.classList.add("char-blank-input");
-//     console.log(charDiv);
-//     wordDiv.appendChild(charDiv);
-//   }
-// }
-
-
-//charArray = ["B", "L", "O", "W","H","O","L","E"]
-
-
-let circleCount = 1; 
+let circleCount = 1;
 playDiv.onclick = async () => {
   const resultedWord = await fetch("https://puzzle.mead.io/puzzle?wordCount=1")
     .then(response => response.json())
@@ -34,38 +11,36 @@ playDiv.onclick = async () => {
 
   wordDiv.innerHTML = "";
 
+  for(let i = 0; i < eventHandlerArray.length; i++){
+    document.removeEventListener('keydown', eventHandlerArray[i]);
+  }
+  
+  eventHandlerArray = [];
+
   const charArray = resultedWord.toUpperCase().split("");
 
   for (let i = 0; i < charArray.length; i++) {
     let charDiv = document.createElement("div");
-
-    //charDiv.innerText = charArray[i];
     charDiv.innerText = "";
     charDiv.classList.add("char-blank-input");
 
     wordDiv.appendChild(charDiv);
 
     const eventHandler = (e) => {
-      //console.log(e);
+      console.log(resultedWord, i);
       const capitalChar = e.key.toUpperCase();
       if (capitalChar === charArray[i]) {
         charDiv.innerText = capitalChar;
-      } 
-      
-      if(i === 0){
-        if(!charArray.includes(capitalChar) && circleCount <=16){
+      }
+
+      if (i === 0) {
+        if (!charArray.includes(capitalChar) && circleCount <= 16) {
           document.querySelector(`#circle${circleCount}`).innerText = capitalChar;
           circleCount++;
         }
       }
-      
-      // if (e.key !== charArray[i]) {
-      //   console.log("not in word");
-      // }
     };
     document.addEventListener("keydown", eventHandler);
-    //document.removeEventListener('keydown', eventHandler)
-
-
+    eventHandlerArray.push(eventHandler);
   }
 }
