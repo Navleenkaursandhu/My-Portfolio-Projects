18760d21d64ca57304304999db01c3fd2c0104f0
Navleen <navleensandhu0211@gmail.com>
2022-08-08 08:54:33 -0700

    get user input and compared it to random word
      - the correct char gets dispalyed in blanks
      - the incorrect gets displayed in the UI

--- projects/HangmanGame/game.js
+++ projects/HangmanGame/game.js
@@ -1,24 +1,71 @@
 const playDiv = document.querySelector("#play");
 const wordDiv = document.querySelector("#word");
 
+// playDiv.onclick = async () => {
+//   const resultedWord = await fetch("https://puzzle.mead.io/puzzle?wordCount=1")
+//     .then(response => response.json())
+//     .then(response => response.puzzle)
+
+//     wordDiv.innerHTML = "";
+
+//   const charArray = resultedWord.toUpperCase().split("");
+
+//   for (let i = 0; i < charArray.length; i++) {
+//     let charDiv = document.createElement("div");
+//     console.log(charDiv);
+
+//     charDiv.innerText = charArray[i];
+//     //charDiv.innerText = "";
+//     charDiv.classList.add("char-blank-input");
+//     console.log(charDiv);
+//     wordDiv.appendChild(charDiv);
+//   }
+// }
+
+
+//charArray = ["B", "L", "O", "W","H","O","L","E"]
+
+
+let circleCount = 1; 
 playDiv.onclick = async () => {
   const resultedWord = await fetch("https://puzzle.mead.io/puzzle?wordCount=1")
     .then(response => response.json())
     .then(response => response.puzzle)
 
-    wordDiv.innerHTML = "";
-  //wordDiv.innerText = resultedWord["puzzle"];
-  const charArray = resultedWord.split("");
-  //wordDiv.innerText = charArray;
+  wordDiv.innerHTML = "";
+
+  const charArray = resultedWord.toUpperCase().split("");
+
   for (let i = 0; i < charArray.length; i++) {
     let charDiv = document.createElement("div");
-    console.log(charDiv);
 
-    charDiv.innerText = charArray[i];
-    //charDiv.innerText = "";
+    //charDiv.innerText = charArray[i];
+    charDiv.innerText = "";
     charDiv.classList.add("char-blank-input");
-    console.log(charDiv);
+
     wordDiv.appendChild(charDiv);
+
+    const eventHandler = (e) => {
+      //console.log(e);
+      const capitalChar = e.key.toUpperCase();
+      if (capitalChar === charArray[i]) {
+        charDiv.innerText = capitalChar;
+      } 
+      
+      if(i === 0){
+        if(!charArray.includes(capitalChar) && circleCount <=16){
+          document.querySelector(`#circle${circleCount}`).innerText = capitalChar;
+          circleCount++;
+        }
+      }
+      
+      // if (e.key !== charArray[i]) {
+      //   console.log("not in word");
+      // }
+    };
+    document.addEventListener("keydown", eventHandler);
+    //document.removeEventListener('keydown', eventHandler)
+
+
   }
 }
-
