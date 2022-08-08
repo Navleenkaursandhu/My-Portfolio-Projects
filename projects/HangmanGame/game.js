6ccb77a05305f8069b071d3c54af20f27581ab0b
Navleen <navleensandhu0211@gmail.com>
2022-08-08 07:22:25 -0700

    formatted js file code

--- projects/HangmanGame/game.js
+++ projects/HangmanGame/game.js
@@ -1,14 +1,6 @@
 const playDiv = document.querySelector("#play");
 const wordDiv = document.querySelector("#word");
 
-// const fetchWordJSON = async() => {
-// const response = await fetch(" https://puzzle.mead.io/puzzle?wordCount=1");
-// const word = await response.json();
-// return word["puzzle"];
-// }
-
-//console.log(fetchWordJSON());
-
 playDiv.onclick = async () => {
   const resultedWord = await fetch("https://puzzle.mead.io/puzzle?wordCount=1")
     .then(response => response.json())
@@ -27,8 +19,6 @@ playDiv.onclick = async () => {
     charDiv.classList.add("char-blank-input");
     console.log(charDiv);
     wordDiv.appendChild(charDiv);
-    
   }
-  
 }
 
