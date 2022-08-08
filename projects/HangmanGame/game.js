d143187fe863948d1860fbdc2f217d860ec7c721
Navleen <navleensandhu0211@gmail.com>
2022-08-08 11:39:54 -0700

    implemented logic if player looses
     - also reset UI when user hits PLAY button

--- projects/HangmanGame/game.js
+++ projects/HangmanGame/game.js
@@ -1,6 +1,6 @@
 const playDiv = document.querySelector("#play");
 const wordDiv = document.querySelector("#word");
-
+const resultDiv = document.querySelector("#result");
 let eventHandlerArray = [];
 
 let circleCount = 1;
@@ -10,11 +10,20 @@ playDiv.onclick = async () => {
     .then(response => response.puzzle)
 
   wordDiv.innerHTML = "";
+  circleCount = 1;
 
-  for(let i = 0; i < eventHandlerArray.length; i++){
+  for (let i = 1; i <= 16; i++) {
+    let circleDiv = document.querySelector(`#circle${i}`)
+    console.log(circleDiv);
+    circleDiv.innerText = "";
+    circleDiv.classList.remove("background-red");
+  }
+  resultDiv.innerText = "";
+
+  for (let i = 0; i < eventHandlerArray.length; i++) {
     document.removeEventListener('keydown', eventHandlerArray[i]);
   }
-  
+
   eventHandlerArray = [];
 
   const charArray = resultedWord.toUpperCase().split("");
@@ -22,7 +31,7 @@ playDiv.onclick = async () => {
   for (let i = 0; i < charArray.length; i++) {
     let charDiv = document.createElement("div");
     charDiv.innerText = "";
-    charDiv.classList.add("char-blank-input");
+    charDiv.classList.add("char-blank-input", "text-center");
 
     wordDiv.appendChild(charDiv);
 
@@ -36,11 +45,29 @@ playDiv.onclick = async () => {
       if (i === 0) {
         if (!charArray.includes(capitalChar) && circleCount <= 16) {
           document.querySelector(`#circle${circleCount}`).innerText = capitalChar;
+          document.querySelector(`#circle${circleCount}`).classList.add("text-center", "background-red");
+
+          if (!charArray.includes(capitalChar) && circleCount === 16) {
+            resultDiv.innerText = "you lost";
+          }
           circleCount++;
         }
+        // if(circleCount !== 16){
+        //   for(let i = 0; i < charArray.length; i++ ){
+        //     if(charArray[i] === document.querySelector("#word:nth-child(i)").innerText);
+
+        //   }
+        //resultDiv.innerText = "you won";
       }
-    };
+      // else if(!charArray.includes(capitalChar) && document.querySelector("#circle15").innerText === capitalChar){
+      //   resultDiv.innerText = "you lost";
+      // }
+      // else {
+      //   resultDiv.innerText = "you won";
+      // }
+    }
     document.addEventListener("keydown", eventHandler);
+
     eventHandlerArray.push(eventHandler);
   }
 }
