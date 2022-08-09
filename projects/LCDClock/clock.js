db1d1da2023cf8b5e847079a26db5d3c71b3ef5a
Navleen <navleensandhu0211@gmail.com>
2022-08-09 12:32:14 -0700

    implemented js logic to display current time on LCD UI
     - also added css animation for blinking effect
     - added indentation and formatted code

--- projects/LCDClock/clock.js
+++ projects/LCDClock/clock.js
@@ -1,27 +1,33 @@
-
+const hourFirstDiv = document.querySelector("#hour-first");
+const hourSecondDiv = document.querySelector("#hour-second");
+const minuteFirstDiv = document.querySelector("#min-first");
+const minuteSecondDiv = document.querySelector("#min-second");
+const secFirstDiv = document.querySelector("#sec-first");
+const secSecondDiv = document.querySelector("#sec-second");
 
 const currentTime = () => {
-const today =  new Date();
-let hour = today.getHours().toString().padStart(2,"0");
-let minute = today.getMinutes().toString().padStart(2,"0");
-let sec = today.getSeconds().toString().padStart(2,"0");
+  const today = new Date();
+  let hour = today.getHours().toString().padStart(2, "0");
+  let minute = today.getMinutes().toString().padStart(2, "0");
+  let sec = today.getSeconds().toString().padStart(2, "0");
 
-let hourArr = hour.split("");
-let minArr = minute.split("");
-let secArr = sec.split("");
+  let hourArr = hour.split("");
+  let minArr = minute.split("");
+  let secArr = sec.split("");
 
-let array  = [...hourArr, ...minArr, ...secArr];
+  let array1 = [...hourArr, ...minArr, ...secArr];
 
-//console.log(array);
+  let array2 = [hourFirstDiv, hourSecondDiv, minuteFirstDiv, minuteSecondDiv, secFirstDiv, secSecondDiv]
 
-array.map(() => {
+  for (let i = 0; i < array2.length; i++) {
+    const digitValue = array1[i];
+    const digitElement = array2[i];
 
-  if(array[0] === '0'){
-    document.querySelector("#hour-first-digit").classList.add("num0-top-segment");
+    for (let j = 0; j <= 9; j++) {
+      digitElement.classList.remove(`num${j}`);
+    }
+    digitElement.classList.add(`num${digitValue}`);
   }
-
-})
 }
 
 setInterval(currentTime, 1000);
-
