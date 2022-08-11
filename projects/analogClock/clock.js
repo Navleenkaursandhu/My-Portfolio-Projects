0484ae1459c294678c0718966ff36a4dafcb4cdb
Navleen <navleensandhu0211@gmail.com>
2022-08-11 08:44:34 -0700

    added js code for clock numbers and changed css styling

--- projects/analogClock/clock.js
+++ projects/analogClock/clock.js
@@ -1,9 +1,31 @@
 const secDiv = document.querySelector("#second-line");
 const minDiv = document.querySelector("#minute-line");
 const hourDiv = document.querySelector("#hour-line");
+const clockDiv = document.querySelector(".clock");
 
 let x1 = 50;
 let y1 = 50;
+
+for(let i = 0; i < 12; i++){
+ if(i % 3 === 0){
+  let clockNums = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  clockNums.setAttribute("x1", Math.sin(Math.PI/6 * i) * 40 + x1);
+  clockNums.setAttribute("y1", -Math.cos(Math.PI/6 * i) * 40 + y1);
+  clockNums.setAttribute("x2", Math.sin(Math.PI/6 * i) * 30 + x1);
+  clockNums.setAttribute("y2", -Math.cos(Math.PI/6 * i) * 30 + y1);
+  clockNums.classList.add("clock-nums");
+  clockDiv.appendChild(clockNums);
+ }
+ else{
+  let clockNums = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  clockNums.setAttribute("x1", Math.sin(Math.PI/6 * i) * 40 + x1);
+  clockNums.setAttribute("y1", -Math.cos(Math.PI/6 * i) * 40 + y1);
+  clockNums.setAttribute("x2", Math.sin(Math.PI/6 * i) * 34 + x1);
+  clockNums.setAttribute("y2", -Math.cos(Math.PI/6 * i) * 34 + y1);
+  clockNums.classList.add("clock-nums");
+  clockDiv.appendChild(clockNums);
+ }
+}
 
 setInterval(() => {
   let currentTime = new Date();
