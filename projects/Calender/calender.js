3d9dfcf944a22ad4adbb07575655271036649a94
Navleen <navleensandhu0211@gmail.com>
2022-08-13 11:08:08 -0700

    implemented logic to:
     - to display days in a month in black color
     - added css styling

--- projects/Calender/calender.js
+++ projects/Calender/calender.js
@@ -8,10 +8,48 @@ let monthDelta = 0;
 let current = new Date();
 
 const updateUI = () => {
+  dateOuterDiv.innerHTML = "";
   const monthBeingDisplayed = new Date(current);
   monthBeingDisplayed.setMonth(monthBeingDisplayed.getMonth() + monthDelta);
 
-  monthYearDiv.innerText= monthBeingDisplayed.toLocaleString('default', { month: "long", year:"numeric"});
+  monthYearDiv.innerText = monthBeingDisplayed.toLocaleString('default', { month: "long", year: "numeric" });
+
+  const monthStartDate = new Date(monthBeingDisplayed);
+  monthStartDate.setDate(1);
+ 
+  const startDate = new Date(monthStartDate); //1
+  startDate.setDate(startDate.getDate() - startDate.getDay());//1-1 = 31 July
+
+  for (let week = 0; week <= 5; week++) {
+    const weekStartDate = new Date(startDate);
+    weekStartDate.setDate(weekStartDate.getDate() + (week * 7));
+    const weekContainerElement = document.createElement('div');
+    appendWeekElements(weekContainerElement, weekStartDate, monthBeingDisplayed.getMonth());
+    dateOuterDiv.appendChild(weekContainerElement);
+  }
+}
+
+const appendWeekElements = (weekContainerElement, weekStartDate, month) => {
+  for (let i = 0; i <= 6; i++) {
+    const dateCorrespondingToDay = new Date(weekStartDate);
+    dateCorrespondingToDay.setDate(dateCorrespondingToDay.getDate() + i);
+
+    const dateDiv = document.createElement("div");
+    dateDiv.classList.add("date-elements");
+
+    dateDiv.innerText = dateCorrespondingToDay.getDate();
+    dateDiv.setAttribute("id", `date${dateDiv.innerText}`);
+    weekContainerElement.appendChild(dateDiv);
+
+    if (dateCorrespondingToDay.getTime() === current.getTime()) {
+      console.log(dateCorrespondingToDay.getDate());
+      dateDiv.classList.add("date-today");
+    }
+    
+    if (dateCorrespondingToDay.getMonth() !== month) {
+      dateDiv.style.color = "grey";
+    }
+  }
 }
 
 afterDiv.onclick = () => {
@@ -29,30 +67,4 @@ todayDiv.onclick = () => {
   updateUI();
 }
 
-
-const weekStartDate = new Date(current);
-
-weekStartDate.setDate(weekStartDate.getDate() - weekStartDate.getDay());
-console.log(weekStartDate);
-
-
-for(let i = 0; i <=6 ; i++){
-  const dateCorrespondingToDay = new Date(weekStartDate);
-  dateCorrespondingToDay.setDate(dateCorrespondingToDay.getDate() + i);
-
-  let dateDiv =  document.createElement("div");
-  dateDiv.innerText = dateCorrespondingToDay.getDate();
-  dateDiv.setAttribute("id", `date${dateDiv.innerText}`);
-  
-  dateOuterDiv.appendChild(dateDiv);
-
-  // to detect today & highlight it
-  if (dateCorrespondingToDay.getTime() === current.getTime()) {
-    console.log(dateCorrespondingToDay.getDate());
-
-    let id = dateCorrespondingToDay.getDate();
-    document.querySelector(`#date${id}`).classList.add("date-today");
-  }
-  console.log(dateCorrespondingToDay);
-}
-
+updateUI();
