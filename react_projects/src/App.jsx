676fd55dd9468cd2b39f55bd178473da1149b2ca
Navleen <navleensandhu0211@gmail.com>
2022-08-23 15:44:47 -0700

    react logic to generate random numbers within a range

--- react_projects/src/App.jsx
+++ react_projects/src/App.jsx
@@ -4,11 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import './App.css'
+import {Main} from './project_q42/main'
 
 const Links = () => {
   return(
     <>
     <h1>My React Projects</h1>
+    <a href="project_q42">Project 42: Generate random number between range</a>
     </>
   )
 }
@@ -20,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<Links />} />
+            <Route path="project_q42" element={<Main/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
