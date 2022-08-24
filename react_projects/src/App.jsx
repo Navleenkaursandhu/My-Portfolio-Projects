3b1c50c044a471e860c1262c94e64b170db5b199
Navleen <navleensandhu0211@gmail.com>
2022-08-24 13:00:48 -0700

    developed logic for rendering strings with random characters
     - user enters the length and count of strings

--- react_projects/src/App.jsx
+++ react_projects/src/App.jsx
@@ -4,11 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import './App.css'
+import {Main} from './project_q44/main'
 
 const Links = () => {
   return(
     <>
-    <h1>My React Projects</h1>
+    <h1>React Projects</h1>
+    <a href="project_q44">Project 44: Generate many strings with random characters</a>
     </>
   )
 }
@@ -20,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<Links />} />
+            <Route path='project_q44' element={<Main/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
