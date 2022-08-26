77d9a7b3f213e9603bbdbb4c2aeb8fb275a36ed6
Navleen <navleensandhu0211@gmail.com>
2022-08-26 15:55:55 -0700

    implemented react logic:
    - to split amount amongst
    - to display amount per person

--- react_projects/src/reactProject_TipCalculator/input.tsx
+++ react_projects/src/reactProject_TipCalculator/input.tsx
@@ -1,7 +1,26 @@
 import image from './assets/image.jpg'
+import React from 'react'
+
+let amount = 0;
 export const Input = () => {
 
+  const [totalCost, setTotalCost] = React.useState("")
+  const [tip, setTip] = React.useState("")
+  const [splitNum, setSplitNum] = React.useState("1")
 
+  const add = () => {
+    setSplitNum(prevNum => (Number(prevNum) + 1).toString())
+  }
+
+  const sub = () => {
+    if (Number(splitNum) > 1) {
+      setSplitNum(prevNum => (Number(prevNum) - 1).toString())
+    }
+  }
+
+  if(Number(splitNum) > 0){
+  amount = (((Number(totalCost) * Number(tip)) / 100) + Number(totalCost)) / Number(splitNum)
+  }
 
   return (
     <>
@@ -15,26 +34,26 @@ export const Input = () => {
           <div className='total-cost'>
             <div>Total Cost in Dollars:</div>
             <input
-              placeholder='Enter bill total'></input>
+              placeholder='Enter bill total' onChange={(e) => setTotalCost(e.target.value)}></input>
           </div>
 
           <div className='tip-amount'>
             <div>Tip %:</div>
             <input
-              placeholder='Enter tip percentage'></input>
+              placeholder='Enter tip percentage' onChange={(e) => setTip(e.target.value)}></input>
           </div>
 
           <div className='split'>
             Split:
-            <div className='add'>+</div>
-            <div>0</div>
-            <div className='subtract'>-</div>
+            <div className='add' onClick={() => add()}>+</div>
+            <div>{splitNum}</div>
+            <div className='subtract' onClick={() => sub()}>-</div>
           </div>
 
           <div className='total-per-person'>Total per Person
           </div>
 
-          <div className='split-amount'></div>
+          <div className='split-amount'>$ {amount.toFixed(2)}</div>
         </div>
       </div>
     </>
