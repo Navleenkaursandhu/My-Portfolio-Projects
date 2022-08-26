c1d714d5348c284d3a13293116f8ada1750137bc
Navleen <navleensandhu0211@gmail.com>
2022-08-26 08:20:51 -0700

    implemented logic for maximum rounds
    - also implemented logic for reset game
    - implemented logic for user and bot score

--- react_projects/src/reactProject_RPS/input.tsx
+++ react_projects/src/reactProject_RPS/input.tsx
@@ -1,43 +1,52 @@
 import './style.css'
 import React from 'react'
 
+const maxRounds = 5
 export const Input = () => {
   const [userChoice, setUserChoice] = React.useState('')
   const [botChoice, setBotChoice] = React.useState('')
-  const[userScore, setUserScore] = React.useState(0)
-  const[botScore, setBotScore] = React.useState(0)
-  const[result, setResult] =  React.useState('Lets Play!')
-  const[round, setRound] = React.useState(1)
-  
+  const [userScore, setUserScore] = React.useState(0)
+  const [botScore, setBotScore] = React.useState(0)
+  const [result, setResult] = React.useState('Lets Play!')
+  const [round, setRound] = React.useState(1)
 
-  
-  
+
+
+
   const onUserChoice = (choice) => {
-    if(round < 3){
-    setRound(prev => prev + 1)
-    }
-    
-    setUserChoice(choice)
+    // if (round === 4) {
+    //   setUserChoice('')
+    //   setBotChoice('')
+    //   setUserScore(0)
+    //   setBotScore(0)
+    //   setResult('Lets Play Again!')
+    // }
+    if (round <= maxRounds) {
+      setRound(prev => prev + 1)
 
-    const array = ['✊', '✋', '✌️']
-    const randomIndex = Math.floor(Math.random() * array.length)
-    const botSelect = array[randomIndex]
-    setBotChoice(botSelect)
+      setUserChoice(choice)
 
-    if((choice === '✊' && botSelect === '✋') ||
-       (choice === '✋' && botSelect === '✌️')||
-       (choice === '✌️' && botSelect === '✊')){
-     setBotScore(prevScore => prevScore + 1)
-     //setResult(prev => prev = 'You Lost:(')
-    }
-    else if(choice === botSelect){
-              //setResult(prev => prev ='Draw!')
-            }
-    else{
-      setUserScore(prevScore => prevScore + 1)
-      //setResult('You Won:)')
+      const array = ['✊', '✋', '✌️']
+      const randomIndex = Math.floor(Math.random() * array.length)
+      const botSelect = array[randomIndex]
+      setBotChoice(botSelect)
+
+      if ((choice === '✊' && botSelect === '✋') ||
+        (choice === '✋' && botSelect === '✌️') ||
+        (choice === '✌️' && botSelect === '✊')) {
+        setBotScore(prevScore => prevScore + 1)
+        //setResult(prev => prev = 'You Lost:(')
+      }
+      else if (choice === botSelect) {
+        //setResult(prev => prev ='Draw!')
+      }
+      else {
+        setUserScore(prevScore => prevScore + 1)
+        //setResult('You Won:)')
+      }
     }
   }
+
 
   // console.log(botScore, userScore)
   // if(botScore > userScore){
@@ -49,7 +58,16 @@ export const Input = () => {
   // else{
   //   setResult('draw')
   // }
-  
+
+  const resetGame = () => {
+    setUserChoice('')
+    setBotChoice('')
+    setUserScore(0)
+    setBotScore(0)
+    setRound(1)
+    setResult('Lets Play!')
+  }
+
   return (
     <>
       <div className='title'>Rock Paper Scissors</div>
@@ -66,8 +84,12 @@ export const Input = () => {
         <div>Bot Score: {botScore}</div>
       </div>
 
+      <div className='reset'>
+        <button onClick={() => resetGame()}>RESET</button>
+      </div>
+
       <div className='result'>{result}</div>
-      <div className='round'>Round <span>{round}</span> of 3</div>
+      <div className='round'>Round <span>{round > maxRounds ? maxRounds : round}</span> of {maxRounds}</div>
     </>
   )
 }
