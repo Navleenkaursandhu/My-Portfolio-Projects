9f2e8096501775a49dcda1f01c96d02011687572
Navleen <navleensandhu0211@gmail.com>
2022-08-28 16:42:27 -0700

    implemented game working logic in react

--- react_projects/src/reactProject _TicTacToe/input.tsx
+++ react_projects/src/reactProject _TicTacToe/input.tsx
@@ -1,13 +1,63 @@
-
+import React from 'react'
 export const Input = () => {
 
+  const [player, setPlayer] = React.useState("x")
+  const [cellValues, setCellValues] = React.useState(
+    [["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+    ])
+
+  const [result, setResult] = React.useState("")
+
+  const displayPlayer = (row, column) => {
+    setPlayer(prevPlayer => {
+      if (prevPlayer === "x") {
+        return "o"
+      }
+      else {
+        return "x"
+      }
+    })
+
+    const newCellVals = [
+      [...cellValues[0]],
+      [...cellValues[1]],
+      [...cellValues[2]],
+    ];
+    if (newCellVals[row][column] === "") {
+      newCellVals[row][column] = player;
+    }
+    setCellValues(newCellVals);
 
 
-  const displayPlayer = () => {
-    console.log("x")
+    if (newCellVals[row][0] === newCellVals[row][1] && newCellVals[row][1] === newCellVals[row][2] && newCellVals[row][0] !== "") {
+      setResult(`${newCellVals[row][0]} Wins!`)
+    }
+
+    else if (newCellVals[0][column] === newCellVals[1][column] && newCellVals[1][column] === newCellVals[2][column]) {
+      setResult(`${newCellVals[0][column]} Wins!`)
+    }
+
+    else if (newCellVals[0][0] === newCellVals[1][1] && newCellVals[1][1] === newCellVals[2][2]) {
+      setResult(`${newCellVals[0][0]} Wins!`)
+    }
+
+    else if (newCellVals[0][2] === newCellVals[1][1] && newCellVals[1][1] === newCellVals[2][0]) {
+      setResult(`${newCellVals[0][2]} Wins!`)
+    }
   }
 
+  const resetGame = () => {
+    setCellValues([["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+    ])
 
+    setPlayer("x")
+
+    setResult("")
+  }
 
   return (
     <>
@@ -16,38 +66,38 @@ export const Input = () => {
       </div>
 
       <div className="center player">Current Player:
-        <div>X</div>
+        <div>{player}</div>
       </div>
 
       <div className="container">
         <table>
           <tbody>
-          <tr>
-            <td onClick={() => displayPlayer()}></td>
-            <td></td>
-            <td></td>
-          </tr>
+            <tr>
+              <td onClick={() => displayPlayer(0, 0)}>{cellValues[0][0]}</td>
+              <td onClick={() => displayPlayer(0, 1)}>{cellValues[0][1]}</td>
+              <td onClick={() => displayPlayer(0, 2)}>{cellValues[0][2]}</td>
+            </tr>
 
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+            <tr>
+              <td onClick={() => displayPlayer(1, 0)}>{cellValues[1][0]}</td>
+              <td onClick={() => displayPlayer(1, 1)}>{cellValues[1][1]}</td>
+              <td onClick={() => displayPlayer(1, 2)}>{cellValues[1][2]}</td>
+            </tr>
 
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+            <tr>
+              <td onClick={() => displayPlayer(2, 0)}>{cellValues[2][0]}</td>
+              <td onClick={() => displayPlayer(2, 1)}>{cellValues[2][1]}</td>
+              <td onClick={() => displayPlayer(2, 2)}>{cellValues[2][2]}</td>
+            </tr>
           </tbody>
         </table>
 
       </div>
 
-      <div className="center result">Result:</div>
+      <div className="center result">Result: {result}</div>
 
       <div className="center">
-        <button className="restart">RESTART</button>
+        <button className="restart" onClick={() => resetGame()}>RESTART</button>
       </div>
     </>
   )
