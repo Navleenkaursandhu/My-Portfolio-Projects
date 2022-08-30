b8beeb1eefd60fccc19b65c765f776e935dc2ea8
Navleen <navleensandhu0211@gmail.com>
2022-08-30 16:27:08 -0700

    implemented core logic of game in react:
    - set 16 trails for user to guess the word on UI
    - the user gets to see if wrong word is attempted inside circles
    - user can reset game by hitting PLAY button

--- react_projects/src/reactProject_Hangman/input.tsx
+++ react_projects/src/reactProject_Hangman/input.tsx
@@ -1,36 +1,89 @@
 import { useEffect, useState } from "react"
 
-let count = 0
-
 export const Input = () => {
 
   const [word, setWord] = useState("")
+  const [arrayStrings, setArrayStrings] = useState([])
+  const[noMatchArray, setNoMatchArray] = useState([])
 
   const displayNewWord = async () => {
     const response = await fetch(" https://puzzle.mead.io/puzzle?wordCount=1")
     const data = await response.json()
-    setWord(data.puzzle)
+    const newWord = data.puzzle.toUpperCase()
+    setWord(newWord)
+
+    setArrayStrings(Array(newWord.length).fill(""))
+
+    setNoMatchArray([])
   }
 
   useEffect(() => {
     displayNewWord()
   }, [])
+  console.log(word)
+
+  const wordArray = word.split("")
+
+  useEffect(() => {
+    const listener = e => {
+      const userAttempt = e.key.toUpperCase();
+      console.log(userAttempt)
+
+    if(wordArray.includes(userAttempt)) {
+      setArrayStrings(wordArray.map((char, index) => {
+        if (userAttempt === char) {
+          console.log(index)
+          return char
+        }
+        else {
+          return arrayStrings[index];
+        }
+      }))
+    }
+    else{
+      setNoMatchArray(noMatchArray.concat(userAttempt))
+    }
+
+console.log(noMatchArray)
+    }
+
+    console.log(arrayStrings)
+
+    document.addEventListener("keydown", listener);
+
+    return () => {
+      document.removeEventListener("keydown", listener)
+    }
+  })
 
   return (
     <>
+      <h1 className="title">HANGMAN</h1>
       <div className="play-btn-container">
         <button className="play" onClick={() => displayNewWord()}>PLAY</button>
       </div>
 
-      <div className="word-container">
-        {Array(word.length).fill(0).map(() => {
-          return <div className="character" key={count++}>
-            h
-            <div className="char"></div>
+      <div className="chances-container">
+        {Array(16).fill('').map((char, i) => {
+          return <div className="chance" key={i}>
+            {noMatchArray[i] || ''}
+            
           </div>
         })}
       </div>
-      
+
+      <div className="word-container">
+        {arrayStrings.map((char, i) => {
+          return <div className="character" key={i}>{char}
+
+          </div>
+        })}
+      </div>
+
+      <div className="result">
+      {noMatchArray[15] && <div>You Lost! It's {word}</div>}
+      {arrayStrings[arrayStrings.length-1]  && <div>You Won:D</div>}
+      </div>
     </>)
 
 }
