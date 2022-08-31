1f4e9bc9612db4d45bf2a7ab017fc62ac61939cf
Navleen <navleensandhu0211@gmail.com>
2022-08-31 13:05:41 -0700

    added logic to make boxes blink in random pattern:
    - used setTimeout method to introduce sleep of 1000 millisecs
    - for array randomization used MAth.random() method

--- react_projects/src/reactProject_SimonSays/input.tsx
+++ react_projects/src/reactProject_SimonSays/input.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react"
+
+const sleep = async (timeToSleep) => new Promise(resolve => setTimeout(resolve, timeToSleep))
+
 export const Input = () => {
 
+  const [order, setOrder] = useState([0, 1, 2, 3, 4])
+  const [boxToColor, setBoxToColor] = useState(-1)
+
+  const randomBlink = async () => {
+
+    let shuffledArray = [...order].sort((a,b) => {
+      return 0.5 - Math.random();
+    })
+    
+    setOrder(shuffledArray)
+
+    for (let i = 0; i < order.length; i++) {
+      setBoxToColor(order[i])
+      await sleep(1000)
+    }
+
+    setBoxToColor(-1)
+  }
 
   return (
     <>
@@ -8,13 +30,15 @@ export const Input = () => {
       </div>
 
       <div className="box-container">
-        {Array(5).fill(0).map(() => {
-          return <div className="box"></div>
+        {Array(5).fill(0).map((value, i) => {
+          return <div className={`box ${boxToColor === i ? "green" : "grey"}`} key={i}>
+
+          </div>
         })}
       </div>
 
       <div className="play-container">
-        <button className="play">PLAY</button>
+        <button className="play" onClick={() => randomBlink()}>PLAY</button>
       </div>
 
       <div className="message">WATCh</div>
