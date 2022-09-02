5d44798085edc314fb8a47cf57d8b00d728fe8b9
Navleen <navleensandhu0211@gmail.com>
2022-09-01 17:36:06 -0700

    fetched data when user clicks search button

--- react_projects/src/reactProject_DictionaryApp/main.tsx
+++ react_projects/src/reactProject_DictionaryApp/main.tsx
@@ -1,41 +1,41 @@
 import './style.css'
-import {Word} from './word'
+import { Word } from './word'
 import { Description } from './description'
 import { useEffect, useState } from 'react'
 export const Main = () => {
 
-const [wordEntered, setWordEntered] = useState("")
-const [wordObject, setWordObject] = useState([])
+  const [wordEntered, setWordEntered] = useState("")
+  const [wordObject, setWordObject] = useState(null)
 
-  const fetchData = async() => {
+  const fetchData = async () => {
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordEntered}`)
     const data = await response.json()
     const wordObject = data[0]
-   console.log(wordObject)
-  //  setWordObject(data[0])
-  //  console.log(wordObject)
+    console.log(wordObject)
+    setWordObject(data[0])
+    console.log(wordObject)
   }
 
- 
- useEffect(() => {
-  if(wordEntered !== ""){
-  fetchData()
-  
+
+  useEffect(() => {
+    if (wordEntered !== "") {
+      fetchData()
+
+    }
+  }, [wordEntered])
+
+
+  const dataFunction = (word) => {
+    setWordEntered(word)
   }
- }, [wordEntered])
 
- 
- const dataFunction = (word) => {
-  setWordEntered(word)
- }
-
-  return(
+  return (
     <div className='dictionary'>
       <div className='main-container'>
-      <Word func={dataFunction}/>
-      <Description/>
+        <Word onChange={dataFunction} />
+        <Description />
       </div>
-  
+
     </div>
   )
 }
