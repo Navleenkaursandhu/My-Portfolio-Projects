6d9f4eec5cf0f56f199fe5bb0dba58ef98f10764
Navleen <navleensandhu0211@gmail.com>
2022-09-02 09:28:03 -0700

    used map() method to display word text and audio:
    - created runAudio function which gets called on audio click
    - displayed text for word entered by the user
    - also added css to word, audio and text jsx elements

--- react_projects/src/reactProject_DictionaryApp/phonetics.tsx
+++ react_projects/src/reactProject_DictionaryApp/phonetics.tsx
@@ -1,34 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const Phonetics = (props) => {
 
   let a = props.list
-console.log(props.list)
- 
-
- const arrayWithPhoneticText = (a || []).filter((object) => object.text && object.audio)
-   
-  
-   console.log(arrayWithPhoneticText)
+  console.log(props.list)
 
 
-  // setPhoneticsArr( phoneticsArr.filter((object) =>{
-  //  if(object.hasOwnProperty("text")){
-  //   return object
-  //  }
-  //  console.log(phoneticsArr)
-  // }))
+  const arrayWithPhoneticText = (a || []).filter((object) => object.text && object.audio)
 
+
+  const runAudio = (audio) => {
+    const playAudio = new Audio(audio)
+    playAudio.play()
+  }
 
   return (
     <>
-    <div className="phonetics-container">hello
-    
+      <div className="phonetics-container">
 
-    
-    </div>
+        {arrayWithPhoneticText.map((object, i) => {
+          return <div className="textAudio-container" key={i}>
+            <div onClick={() => runAudio(object.audio)} className="audio"><span className=" audio material-symbols-outlined">
+              volume_down_alt
+            </span></div>
+            <div className="text"> {object.text}</div>
+          </div>
+        })}
 
-    
+      </div>
+
+
     </>
   )
 }
