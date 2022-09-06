bd1b29ed29f65f0c8ea942639c5d708967295aed
Navleen <navleensandhu0211@gmail.com>
2022-09-06 09:03:13 -0700

    fetched data from API:
    - current, hourly & daily weather data fetched from API
    - created array of hourlyData including multiple weather objects
      - time, temperature, windspeed, rain, snowfall, weathercode etc

--- react_projects/src/react_project_WeatherApp/main.tsx
+++ react_projects/src/react_project_WeatherApp/main.tsx
@@ -19,24 +19,77 @@ export const Main = () => {
 
     const lat = currLocation.coords.latitude
     const lon = currLocation.coords.longitude
+    const zone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=${true}&hourly=temperature_2m,windspeed_10m,rain,snowfall,weathercode,apparent_temperature&daily(timezone)=temperature_2m_max,temperature_2m_min,windspeed_10m_max,weathercode,rain_sum,snowfall_sum,showers_sum,apparent_temperature_max,apparent_temperture_min`)
+    const hourlyFields = [
+      "temperature_2m",
+      "windspeed_10m",
+      "rain,snowfall",
+      "weathercode",
+      "apparent_temperature"
+    ]
+
+    const dailyFields = [
+      "temperature_2m_max",
+      "temperature_2m_min",
+      "windspeed_10m_max",
+      "weathercode",
+      "rain_sum",
+      "snowfall_sum",
+      "showers_sum",
+      "apparent_temperature_max",
+      "apparent_temperature_min"
+    ]
+
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=${true}&hourly=${hourlyFields.join(",")}&timezone=${zone}&daily=${dailyFields.join(",")}`)
     const data = await response.json()
     console.log(data)
 
     const finalData = {
       raw: data,
-      hourlyData: data.hourly.time.map((time, i) => ({
-        time: new Date(time),
+      hourlyData: data.hourly.time.map((timeStr, i) => ({
+        time: {
+          value: new Date(timeStr),
+          unit: data.hourly_units.time
+        },
+
         temperature: {
           value: data.hourly.temperature_2m[i],
           unit: data.hourly_units.temperature_2m,
         },
+ 
+        hourlyWindspeed: {
+          value: data.hourly.windspeed_10m[i],
+          unit: data.hourly_units.windspeed_10m
+        },
+
+        hourlyRain: {
+          value: data.hourly.rain[i],
+          unit: data.hourly_units.rain
+        },
+
+        hourlySnowfall: {
+          value: data.hourly.snowfall[i],
+          unit: data.hourly_units.snowfall
+        },
+
+        hourlyWeathercode: {
+          value: data.hourly.weathercode[i],
+          unit: data.hourly_units.weathercode
+        },
+
+        hourlyApparentTemperature: {
+          value: data.hourly.apparent_temperature[i],
+          unit: data.hourly_units.apparent_temperature
+        }
+
+
       }))
 
-      //currentData: data.current_weather
+
     };
     console.log(finalData)
+
   }
 
 
