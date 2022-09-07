f7b3e677bafe469557387ba7c5814cb079748448
Navleen <navleensandhu0211@gmail.com>
2022-09-06 20:57:03 -0700

    displayed day and date using date-fns npm package on UI

--- react_projects/src/react_project_WeatherApp/dailyWeather.tsx
+++ react_projects/src/react_project_WeatherApp/dailyWeather.tsx
@@ -1,11 +1,22 @@
+import { format } from "date-fns"
+
 export const DailyWeather = (props) => {
 
-
-
+// const arrayDailyData = props.dailyData
+// console.log(props.dailyData)
+// console.log(arrayDailyData[0].time.value)
 
   console.log(props.dailyData)
   return(
   <>
-  <div></div>
+  {props.dailyData &&
+  <div className="daily-weather">
+    <div className="select-date">
+      <div className="day-before"></div>
+      <div>{format(props.dailyData[0].time.value,"ccc, do MMM yyyy")}</div>
+      <div className="day-after"></div>
+    </div>
+  </div>
+}
   </>)
 }
