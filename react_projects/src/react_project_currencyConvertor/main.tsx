ff481f883b125429a79134b6bbb522d3344c0429
Navleen <navleensandhu0211@gmail.com>
2022-09-09 16:38:05 -0700

    implemented logic where user is able to select option:
    - from option & to currency option
    - displayed the description of user selected currency on UI

--- react_projects/src/react_project_currencyConvertor/main.tsx
+++ react_projects/src/react_project_currencyConvertor/main.tsx
@@ -1,12 +1,45 @@
+import { useEffect, useState } from 'react'
 import './style.css'
 export const Main = () => {
 
+  const [symbols, setSymbols] = useState({})
+  const [selectedFromOption, setSelectedFromOption] = useState("")
+  const [selectedToOption, setSelectedToOption] = useState("")
+
+  const fetchData = async () => {
+    const response = await fetch("https://api.exchangerate.host/symbols")
+    const data = await response.json()
+    setSymbols(data.symbols)
+  }
+
+  console.log(symbols)
+  console.log(Object.keys(symbols))
+  //console.log(selectedOption)
+
+  const keyArray = Object.keys(symbols)
+
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+  const displayDescriptionFrom = (e) => {
+    setSelectedToOption(e.target.value)
+  }
+
+  const displayDescriptionTo = (e) => {
+    setSelectedFromOption(e.target.value)
+  }
+
+  console.log(selectedToOption)
+  console.log(selectedFromOption)
+
+  // console.log(symbols[selectedFromOption].description)
+  // console.log(symbols[selectedToOption].description)
 
 
 
-
-
-  
   return (
     <div className="currency-container">
       <div className="convertor">
@@ -23,12 +56,38 @@ export const Main = () => {
         </div>
 
         <div className='select-currency-container'>
-          <select className='from'>USD</select>
+          <select onChange={(e) => displayDescriptionFrom(e)} className='from'>
+            <option>Select</option>
+            {keyArray.map((key, i) => {
+              return <option key={i} value={symbols[key].code} className='option'>{symbols[key].code}
+              </option>
+            })}
+
+
+          </select>
+
           <span className="from-to material-symbols-rounded">
             swap_horiz
           </span>
-          <select className='to'>CAD</select>
+
+          <select onChange={(e) => displayDescriptionTo(e)} className='to'>
+            <option >Select</option>
+            {keyArray.map((key, i) => {
+              return <option value={symbols[key].code} key={i} className='option'>{symbols[key].code}</option>
+            })}
+          </select>
         </div>
+
+        <div className='currency-description'>
+          <div className='from-description'>
+            {selectedToOption && <div>{symbols[selectedToOption].description}</div>}
+          </div>
+
+          <div className='to-description'>
+            {selectedFromOption && <div>{symbols[selectedFromOption].description}</div>}
+          </div>
+        </div>
+
 
         <div className='unit-value-container'>
           <div className='unit-before'>1 RUPEE</div>
