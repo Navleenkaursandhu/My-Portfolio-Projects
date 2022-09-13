575b72e36048dbf0d3154e818e257811a58487c6
Navleen <navleensandhu0211@gmail.com>
2022-09-13 13:26:19 -0700

    added local storage feature in app
    - the user is able to retrieve previous text based on date

--- react_projects/src/react_project_DailyDiary/DiaryPage.tsx
+++ react_projects/src/react_project_DailyDiary/DiaryPage.tsx
@@ -1,24 +1,33 @@
 import { formatISO } from 'date-fns'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import image from './assets/image.png'
 export const DiaryPage = (props) => {
-  const [object, setObject] = useState({})
-  console.log(props.currentDate)
+  const [object, setObject] = useState(() => {
+    const saved = localStorage.getItem('data')
+    console.log('SAVED', saved)
+    return JSON.parse(saved) || {}
+  })
 
+  const dateKey = formatISO(props.currentDate, { representation: 'date' })
+  console.log(props.currentDate)
   const createObject = (e) => {
     setObject((prev) => {
       const newVal = { ...prev }
-      newVal[formatISO(props.currentDate)] = e.target.value
+      newVal[dateKey] = e.target.value
       return newVal
     }
     )
   }
-  console.log(object)
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(object))
+  }, [object])
+
   return (
     <div className='diary-page'>
       <img className="image-diary-page" src={image} />
       <div className='textarea-container'>
-        <textarea value={object[formatISO(props.currentDate)] || ''} onChange={(e) => createObject(e)} />
+        <textarea value={object[dateKey] || ''} onChange={(e) => createObject(e)} />
       </div>
     </div>
   )
