ed8dc8089252074d15fc4b5fe56e18c8edcbb94f
Navleen <navleensandhu0211@gmail.com>
2022-09-13 19:37:07 -0700

    added some css styling to buttons and table

--- react_projects/src/react_project_Timesheet/Header.tsx
+++ react_projects/src/react_project_Timesheet/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <>
         <div className='header-container'>
-          <div className="sign-in-out">SIGN IN</div>
+          <button className="sign-in-out">SIGN IN</button>
           <input className="input-task" placeholder="Enter your task here"></input>
           <button className="add-task-button">ADD TASK</button>
         </div>
