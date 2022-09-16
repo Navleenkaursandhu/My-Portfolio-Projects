c8f7852753f78b1e54f4419dd2fdd53b5c38ea86
Navleen <navleensandhu0211@gmail.com>
2022-09-16 12:15:09 -0700

    added styling for pre and post wedding event details

--- react_projects/src/react_project_WeddingCard/Information.tsx
+++ react_projects/src/react_project_WeddingCard/Information.tsx
@@ -1,11 +1,11 @@
-
+import dividerImage from './assets/divider.png'
 export const Information = () => {
   return (
     <div className="information-container">
       <div className="details">DETAILS</div>
 
       <div className="information">
-        <div className="family-info">FAMILY DETAILS</div>
+        <div className="familydetails-heading">FAMILY DETAILS</div>
         <div className="family-information">
           <div className="bride-side">
             <div>BRIDE  SIDE</div>
@@ -27,7 +27,7 @@ export const Information = () => {
             <div className="bride-address">Address:
               <input></input>
             </div>
-            <div className="bride-rsvp">RSVP Cell No:
+            <div className="bride-rsvp">RSVP Contact No:
               <input></input>
             </div>
           </div>
@@ -52,27 +52,96 @@ export const Information = () => {
             <div className="groom-address">Address:
               <input></input>
             </div>
-            <div className="groom-rsvp">RSVP Cell No:
+            <div className="groom-rsvp">RSVP Contact No:
               <input></input>
             </div>
           </div>
         </div>
 
-        <div className="weddingday-info">WEDDING DAY DETAILS</div>
-        <div className="bride-groom-side-weddingday-info">
-          <div className="date">Date:
-            <input></input>
+        <div className='divider-container'>
+          <img className="divider" src={dividerImage} />
+        </div>
+
+        <div className="weddingdaydetails-heading">WEDDING DAY DETAILS</div>
+        <div className="weddingday-information">
+          <div className="wedding-date-day">
+            <div className="wedding-date">Date:
+              <input></input>
+            </div>
+            <div className="wedding-day">Day:
+              <input></input>
+            </div>
           </div>
-          <div className="day">Day:
-            <input></input>
-          </div>
-          <div className="time">Time:
-            <input></input>
-          </div>
-          <div className="venue">Venue:
-            <input></input>
+          <div className="wedding-time-venue">
+            <div className="wedding-time">Time:
+              <input></input>
+            </div>
+            <div className="wedding-venue">Venue:
+              <input></input>
+            </div>
           </div>
         </div>
+
+        <div className='divider-container'>
+          <img className="divider" src={dividerImage} />
+        </div>
+
+        <div className='prewedding-eventdetails-heading'>PRE-WEDDING EVENT DETAILS</div>
+        <div className='preweddingevents-information'>
+
+          <div className='prewedding-eventtype'>Event Type:
+            <input></input>
+          </div>
+
+          <div className='preweddingevent-date-day'>
+            <div className='preweddingevent-date'>Date:
+              <input></input>
+            </div>
+            <div className='preweddingevent-day'>Day:
+              <input></input>
+            </div>
+          </div>
+
+          <div className='preweddingevent-time-venue'>
+            <div className='preweddingevent-time'>Time:
+              <input></input>
+            </div>
+            <div className='preweddingevent-venue'>Venue:
+              <input></input>
+            </div>
+          </div>
+        </div>
+
+        <div className='divider-container'>
+          <img className="divider" src={dividerImage} />
+        </div>
+
+        <div className='postwedding-eventdetails-heading'>POST-WEDDING EVENT DETAILS</div>
+        <div className='postweddingevents-information'>
+
+          <div className='postwedding-eventtype'>Event Type:
+            <input></input>
+          </div>
+
+          <div className='postweddingevent-date-day'>
+            <div className='postweddingevent-date'>Date:
+              <input></input>
+            </div>
+            <div className='postweddingevent-day'>Day:
+              <input></input>
+            </div>
+          </div>
+
+          <div className='postweddingevent-time-venue'>
+            <div className='postweddingevent-time'>Time:
+              <input></input>
+            </div>
+            <div className='postweddingevent-venue'>Venue:
+              <input></input>
+            </div>
+          </div>
+        </div>
+
       </div>
       {/* <div>DATE and DAY</div>
       <div>TIME</div>
