d545495f2755fa880652c54a4a85ab3cbd0bcf3b
Navleen <navleensandhu0211@gmail.com>
2022-09-27 16:51:10 -0700

    added tailwind css

--- react_projects/src/project_q46/main.tsx
+++ react_projects/src/project_q46/main.tsx
@@ -1,7 +1,7 @@
 import { Input } from './input'
 export const Main = () => {
   return (
-  <>
-  <Input/>
-  </>)
+    <div className='flex justify-center items-center h-screen flex-wrap'>
+      <Input />
+    </div>)
 }
