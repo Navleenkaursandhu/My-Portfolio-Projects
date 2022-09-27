6be9f7ea2045f176506e4a7418927d31770d9528
Navleen <navleensandhu0211@gmail.com>
2022-09-27 14:09:04 -0700

    did formatting

--- react_projects/src/reactProject_DictionaryApp/word.tsx
+++ react_projects/src/reactProject_DictionaryApp/word.tsx
@@ -6,7 +6,7 @@ export const Word = (props) => {
   return (
     <>
       <div className="w-[44%] h-full flex flex-col bg-[#f4e8d8] bg-no-repeat bg-contain bg-bottom border-[10px] border-[#2c2f62] border-r-0" style={{ backgroundImage: `url(${image})` }}>
-        <div className=' text-[50px] text-center text-[#312e5c] flex items-center pt-[30px]'>One App Instead Of 1000 Words</div>
+        <div className='text-[50px] text-center text-[#312e5c] flex items-center pt-[30px]'>One App Instead Of 1000 Words</div>
         <div className='flex items-center justify-center p-[70px] text-[#474b74] '>
           <input className='text-[24px] border-[#474b74] rounded-[10px] p-[10px] w-[80%] font-applyCormorant text-[#3e4579]' type="text" placeholder='search for a word' onChange={(e) => setEnteredWord(e.target.value)} />
           <span onClick={() => props.onChange(enteredWord)} className="material-symbols-rounded flex flex-col justify-center ml-[-48px] text-[40px] hover:cursor-pointer">search</span>
