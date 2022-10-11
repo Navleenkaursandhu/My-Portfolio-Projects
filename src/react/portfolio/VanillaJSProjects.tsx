d69f9be0c9384fd3865d72c13c9bae9769dcc17c
Navleen <navleensandhu0211@gmail.com>
2022-10-11 08:14:08 -0700

    did some changes to description

--- src/react/portfolio/VanillaJSProjects.tsx
+++ src/react/portfolio/VanillaJSProjects.tsx
@@ -84,8 +84,8 @@ const projects = [
       Please note that in order to play this game you need to press your keyboard alphabets to take actions during fight. The rules
       are pretty simple. The game has 3 rounds in total. To start the game Player A takes the first round followed by player B and so forth. It will be the player&apos;s choice whether
       he/she wants to hit the opponent or take a lifeline during his/her round (remember there is no point of taking
-      a lifeline in your first round as it&apos;s already 100%). On hit, opponents health will reduce by a random number (the damage will be in the form of red bar)
-      and on selecting lifeline the current player&apos;s health increases by random number (health is in the form of green bar). After
+      a lifeline in your first round as it&apos;s already 100%). On hit, opponents health will reduce by a random number (the damage will be reflected by red bar)
+      and on selecting lifeline the current player&apos;s health increases by random number (health is reflected by green bar). After
       3rd round the player with good health wins the battle.
     </div>,
     skills: ['JavaScript', 'CSS', 'HTML', 'Git', 'Github', 'VS Code'],
