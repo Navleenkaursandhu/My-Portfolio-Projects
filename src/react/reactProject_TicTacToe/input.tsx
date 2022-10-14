354eede057185c205ecd231f76371e3ec338b793
Navleen <navleensandhu0211@gmail.com>
2022-10-14 13:37:12 -0700

    changed css styling

--- src/react/reactProject_TicTacToe/input.tsx
+++ src/react/reactProject_TicTacToe/input.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { buttonShadowEffect } from '../common/tailwind_constants'
 export const Input = () => {
-  const [player, setPlayer] = React.useState('x')
+  const [player, setPlayer] = React.useState('X')
   const [cellValues, setCellValues] = React.useState(
     [['', '', ''],
       ['', '', ''],
@@ -11,10 +12,10 @@ export const Input = () => {
 
   const displayPlayer = (row, column) => {
     setPlayer(prevPlayer => {
-      if (prevPlayer === 'x') {
-        return 'o'
+      if (prevPlayer === 'X') {
+        return 'O'
       } else {
-        return 'x'
+        return 'X'
       }
     })
 
@@ -48,7 +49,7 @@ export const Input = () => {
         ['', '', '']
       ])
 
-    setPlayer('x')
+    setPlayer('X')
 
     setResult('')
   }
@@ -56,32 +57,32 @@ export const Input = () => {
   return (
     <>
       <div className="flex justify-center">
-        <div className="w-[50%] text-black text-[54px] mt-[30px] text-center">Tic Tac Toe</div>
+        <div className="w-[50%] text-black text-[54px] mt-[20px] text-center">Tic Tac Toe</div>
       </div>
 
       <div className="flex justify-center text-[40px] p-[20px]">Current Player:&nbsp;
         <div>{player}</div>
       </div>
 
-      <div className="flex justify-center">
+      <div className="flex justify-center mb-6">
         <table className='border-collapse'>
           <tbody>
             <tr>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(0, 0)}>{cellValues[0][0]}</td>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(0, 1)}>{cellValues[0][1]}</td>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(0, 2)}>{cellValues[0][2]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(0, 0)}>{cellValues[0][0]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(0, 1)}>{cellValues[0][1]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(0, 2)}>{cellValues[0][2]}</td>
             </tr>
 
             <tr>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(1, 0)}>{cellValues[1][0]}</td>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(1, 1)}>{cellValues[1][1]}</td>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(1, 2)}>{cellValues[1][2]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(1, 0)}>{cellValues[1][0]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(1, 1)}>{cellValues[1][1]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(1, 2)}>{cellValues[1][2]}</td>
             </tr>
 
             <tr>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(2, 0)}>{cellValues[2][0]}</td>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(2, 1)}>{cellValues[2][1]}</td>
-              <td className='w-[130px] h-[130px] text-[80px] text-center border-2 border-black' onClick={() => displayPlayer(2, 2)}>{cellValues[2][2]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(2, 0)}>{cellValues[2][0]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(2, 1)}>{cellValues[2][1]}</td>
+              <td className='w-[130px] h-[130px] text-[50px] text-center border-4' onClick={() => displayPlayer(2, 2)}>{cellValues[2][2]}</td>
             </tr>
           </tbody>
         </table>
@@ -90,7 +91,7 @@ export const Input = () => {
       <div className="flex justify-center text-[38px] p-[4px]">{result}</div>
 
       <div className="flex justify-center">
-        <button className="text-[28px] p-[4px] rounded-[10px] m-[4px] bg-black text-white px-[10px] hover:shadow-[5px_5px_10px_10px_#d1cdcd]" onClick={() => resetGame()}>RESTART</button>
+        <button className={`text-3xl px-6 rounded-[10px] bg-zinc-300 shadow-zinc-400 hover:shadow-zinc-400 py-2 ${buttonShadowEffect}`} onClick={() => resetGame()}>RESTART</button>
       </div>
     </>
   )
