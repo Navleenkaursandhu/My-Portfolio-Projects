6fa078c98326743755448dad7e0f0c45e42fb2ee
Navleen <navleensandhu0211@gmail.com>
2022-12-21 14:30:17 -0800

    added state variable for random sorting of cards   - when user lands directly on DOM and upon clicking restart button

--- src/react/react_project_CardMatchGame/CardMatchGame.tsx
+++ src/react/react_project_CardMatchGame/CardMatchGame.tsx
@@ -1,19 +1,32 @@
 import { cards } from './cardsData'
 import { buttonShadowEffect } from '../common/tailwind_constants'
+import { useEffect, useState } from 'react'
 
 export const CardMatchGame = () => {
-  const randomlySortedCardsDataArray = cards.sort(() => 0.5 - Math.random())
+  const [currentCardsData, setCurrentCardsData] = useState(cards)
+
+  // const randomlySortedCardsDataArray = cards.sort(() => 0.5 - Math.random())
+
+  const randomlySortedCardsDataArray = () => {
+    setCurrentCardsData(prev => [...prev].sort(() => 0.5 - Math.random()))
+  }
+
+  useEffect(() => setCurrentCardsData(prev => [...prev].sort(() => 0.5 - Math.random())), [])
+
+
+
   return (
     <>
       <div className='h-screen flex flex-col gap-4 justify-center items-center font-applyConcert text-2xl'>
         <h1 className='text-3xl text-indigo-500'>CARD MATCH GAME</h1>
-        <button className={`${buttonShadowEffect} bg-indigo-500 text-white rounded-md px-3 py-1.5`}>RESTART</button>
+        <button onClick={() => randomlySortedCardsDataArray()}
+          className={`${buttonShadowEffect} bg-indigo-500 text-white rounded-md px-3 py-1.5`}>RESTART</button>
         <div>0/52 CARDS MATCHED</div>
         <div className='w-1/2 flex flex-wrap gap-2 justify-center'>
-          {randomlySortedCardsDataArray.map((cardDetail, i) => {
-            const color = cardDetail.color === "red" ? "text-red-600": "text-black"
-            console.log(color)
-            return <div className={`w-20 py-4 px-2 rounded-md `}>
+          {currentCardsData.map((cardDetail, i) => {
+            const color = cardDetail.color === "red" ? "text-red-600" : "text-black"
+
+            return <div className={`w-20 py-4 px-2 rounded-md border border-1`}>
               <div className='flex flex-col items-center'>
                 <div className={`${color}`}>{cardDetail.num}</div>
                 <div className={`${color}`}>{cardDetail.type}</div>
