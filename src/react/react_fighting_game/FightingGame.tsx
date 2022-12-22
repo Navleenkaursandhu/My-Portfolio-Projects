8f634b76b93240131deee9e82b7bd173445f852c
Navleen <navleensandhu0211@gmail.com>
2022-12-22 14:36:13 -0800

    created UI displaying 2 players A & B and a playground

--- src/react/react_fighting_game/FightingGame.tsx
+++ src/react/react_fighting_game/FightingGame.tsx
@@ -1,7 +1,27 @@
+import playerA from './assets/playerA.gif'
+import playerB from './assets/playerB.gif'
+
 export const FightingGame = () => {
   return (
     <>
-    <div>game</div>
+      <div className="h-screen flex flex-col justify-center items-center gap-4 border border-black">
+        <div>
+          lifelines
+        </div>
+
+        <div>
+          <div className='flex justify-center w-full'>
+            <img className=' sm:h-auto sm:w-auto h-32 sm:ml-16' src={playerA} />
+            <img className=' sm:h-auto sm:w-auto h-32' src={playerB} />
+          </div>
+          <div className='-mt-3.5 rounded-[50%] h-8 bg-gradient-to-b from-lime-400 to-amber-200'></div>
+        </div>
+
+        <div>
+          buttons
+        </div>
+      </div>
+      
     </>
   )
 }
