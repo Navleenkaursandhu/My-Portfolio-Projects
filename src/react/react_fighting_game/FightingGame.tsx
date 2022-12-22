d7d7afa4f70054b0f56e38ffcf43ecece0b75240
Navleen <navleensandhu0211@gmail.com>
2022-12-22 14:51:39 -0800

    added restart button and msg displaying let's play to the user

--- src/react/react_fighting_game/FightingGame.tsx
+++ src/react/react_fighting_game/FightingGame.tsx
@@ -1,10 +1,11 @@
 import playerA from './assets/playerA.gif'
 import playerB from './assets/playerB.gif'
+import { buttonShadowEffect } from '../common/tailwind_constants'
 
 export const FightingGame = () => {
   return (
     <>
-      <div className="h-screen flex flex-col justify-center items-center gap-4 border border-black">
+      <div className="h-screen flex flex-col justify-center items-center gap-4 border border-black sm:text-xl">
         <div>
           lifelines
         </div>
@@ -14,14 +15,15 @@ export const FightingGame = () => {
             <img className=' sm:h-auto sm:w-auto h-32 sm:ml-16' src={playerA} />
             <img className=' sm:h-auto sm:w-auto h-32' src={playerB} />
           </div>
-          <div className='-mt-3.5 rounded-[50%] h-8 bg-gradient-to-b from-lime-400 to-amber-200'></div>
+          <div className='-mt-3.5 rounded-[50%] h-8 bg-gradient-to-b from-emerald-100 via-lime-400 to-amber-200'></div>
         </div>
 
-        <div>
-          buttons
+        <div className='flex flex-col gap-6 items-center'>
+          <button className={`${buttonShadowEffect} bg-indigo-400 px-5 py-1.5 rounded-md text-white font-semibold`}>RESTART</button>
+          <div>LET&apos;S PLAY!</div>
         </div>
       </div>
-      
+
     </>
   )
 }
