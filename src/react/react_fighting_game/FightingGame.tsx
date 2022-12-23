84d1f552c20cb2ad8de9bb2d29565632fa54a121
Navleen <navleensandhu0211@gmail.com>
2022-12-23 14:01:02 -0800

    implemented logic to set 3 rounds only,once the round is 0, the game is over

--- src/react/react_fighting_game/FightingGame.tsx
+++ src/react/react_fighting_game/FightingGame.tsx
@@ -3,38 +3,51 @@ import playerB from './assets/playerB.gif'
 import attackImage from './assets/punch.png'
 import lifelineImage from './assets/first-aid-kit.png'
 import { buttonShadowEffect } from '../common/tailwind_constants'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const FightingGame = () => {
   const [round, setRound] = useState(3)
+
+
   const [isPlayerATurn, setIsPlayerATurn] = useState(true)
   const [playerAHealth, setPlayerAHealth] = useState(100)
   const [playerBHealth, setPlayerBHealth] = useState(100)
 
+  const randomNum = (Math.floor(Math.random() * (10)) + 20)
+
   const reducePlayerBHealth = () => {
     setIsPlayerATurn(prev => !prev)
-    setPlayerBHealth(prev => prev - (Math.floor(Math.random() * (10)) + 20))
-
+    setPlayerBHealth(prev => prev - randomNum)
   }
 
   const reducePlayerAHealth = () => {
     setIsPlayerATurn(prev => !prev)
-    setPlayerAHealth(prev => prev - (Math.floor(Math.random() * (10)) + 20))
+    if (round > 0) {
+      setRound(prev => prev - 1)
+    }
+    setPlayerAHealth(prev => prev - randomNum)
   }
 
   const addHealthToPlayerA = () => {
     setIsPlayerATurn(prev => !prev)
-    setPlayerAHealth(prev => prev + (Math.floor(Math.random() * (10)) + 20))
+    if (playerAHealth !== 100) {
+      setPlayerAHealth(prev => prev + randomNum)
+    }
   }
 
   const addHealthToPlayerB = () => {
     setIsPlayerATurn(prev => !prev)
-    setPlayerBHealth(prev => prev + (Math.floor(Math.random() * (10)) + 20))
+    if (round > 0) {
+      setRound(prev => prev - 1)
+    }
+    if (playerBHealth !== 100) {
+      setPlayerBHealth(prev => prev + randomNum)
+    }
   }
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center gap-6 lg:text-2xl sm:text-xl text-sm sm:p-4 p-1.5 font-semibold">
+      <div className="flex flex-col justify-center items-center gap-5 lg:text-2xl sm:text-xl text-sm sm:p-4 p-1.5 font-semibold">
         <h1>FIGHTING GAME</h1>
         <div className='flex flex-row sm:gap-4 gap-1 sm:w-4/5 w-full'>
           <div className='flex-1'>
@@ -93,28 +106,28 @@ export const FightingGame = () => {
         </div>
 
         <div className='grid grid-cols-2 sm:gap-x-24 gap-x-12 lg:w-1/2 w-full'>
-          <div className='h-[96px] flex gap-4 items-center justify-center'>
+          <div className='h-[80px] flex gap-4 items-center justify-center'>
             <div>ATTACK</div>
             <button onClick={() => isPlayerATurn && reducePlayerBHealth()} className={`${buttonShadowEffect} sm:w-16 w-8 px-2 flex justify-center items-center bg-gradient-to-r from-rose-100  to-red-300 rounded-md shadow-[4px_4px_0px_0px_#df8889] hover:shadow-[2px_2px_0px_0px_#df8889]`}>
               <img src={attackImage} />
             </button>
           </div>
 
-          <div className='h-[96px] gap-4 flex items-center justify-center'>
+          <div className='h-[80px] gap-4 flex items-center justify-center'>
             <button onClick={() => !isPlayerATurn && reducePlayerAHealth()} className={`${buttonShadowEffect} sm:w-16 w-8 px-2 bg-gradient-to-r from-red-300  to-rose-100 flex justify-center items-center rounded-md shadow-[4px_4px_0px_0px_#df8889] hover:shadow-[2px_2px_0px_0px_#df8889]`}>
               <img className='-scale-x-100' src={attackImage} />
             </button>
             <div>ATTACK</div>
           </div>
 
-          <div className='h-[96px] flex gap-4 items-center justify-center'>
+          <div className='h-[80px] flex gap-4 items-center justify-center'>
             <div>LIFELINE</div>
             <button onClick={() => isPlayerATurn && addHealthToPlayerA()} className={`${buttonShadowEffect} sm:w-16 w-8 px-2.5 bg-gradient-to-b from-blue-100  to-blue-300 rounded-md shadow-[4px_4px_0px_0px_#7eb6f5] hover:shadow-[2px_2px_0px_0px_#7eb6f5]`}>
               <img src={lifelineImage} />
             </button>
           </div>
 
-          <div className='h-[96px] flex gap-4 items-center justify-center'>
+          <div className='h-[80px] flex gap-4 items-center justify-center'>
             <button onClick={() => !isPlayerATurn && addHealthToPlayerB()} className={`${buttonShadowEffect} sm:w-16 w-8 px-2.5 bg-gradient-to-b from-blue-100  to-blue-300 rounded-md shadow-[4px_4px_0px_0px_#7eb6f5] hover:shadow-[2px_2px_0px_0px_#7eb6f5]`}>
               <img src={lifelineImage} />
             </button>
@@ -122,9 +135,12 @@ export const FightingGame = () => {
           </div>
         </div>
 
-        <div className='flex flex-col gap-4 items-center'>
+        <div>PLAYER A WINS!</div>
+
+        <div className='flex flex-col gap-2 items-center'>
           <button className={`${buttonShadowEffect} bg-indigo-400 px-5 py-1.5 rounded-md text-white`}>RESTART</button>
-          <div>LET&apos;S PLAY!</div>
+          {!!round && <div>LET&apos;S PLAY!</div>}
+          {!round && <div>GAME OVER</div>}
         </div>
       </div>
 
