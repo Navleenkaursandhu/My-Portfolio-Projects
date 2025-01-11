dddbeafe04fa86a1a0b3abb88ef052a081e5e5cf
Satnam Singh Brar <brar.satnam1995@gmail.com>
2025-01-11 15:59:22 -0800

    Update the portfolio to highlight my teaching experience

--- src/react/portfolio/Intro.tsx
+++ src/react/portfolio/Intro.tsx
@@ -1,8 +1,5 @@
 import portfolioImage from './assets/portfolio-image.png'
 import githubImage from './assets/mygithub.png'
-import linkedInImage from './assets/linkedin.png'
-import resumeImage from './assets/resume.png'
-import myResume from './assets/resume.pdf'
 import { buttonShadowEffect } from '../common/tailwind_constants'
 export const Intro = () => {
   return (
@@ -12,9 +9,14 @@ export const Intro = () => {
           <div className='lg:p-8 md:p-8 p-2 flex-1 flex items-center'>
             <div>
               <div className='font-extrabold lg:text-6xl md:text-5xl text-4xl text-[#371064]'>Navleen Kaur Brar</div>
-              <div className='lg:text-2xl md:text-xl text-lg mt-4 text-gray-400 font-bold'>FRONT-END WEB DEVELOPER + DESIGNER</div>
-              <div className='lg:text-2xl md:text-xl text-lg mt-8 text-[#371064]'>An aspiring Front-End Web Developer and Designer passionate about designing interactive web applications
-                with strong organizational and problem solving skills.</div>
+              <div className='lg:text-2xl md:text-xl text-lg mt-4 text-gray-400 font-bold'>EDUCATOR</div>
+              <div className='lg:text-2xl md:text-xl text-lg mt-8 text-[#371064]'>
+                An aspiring middle years and high school teacher passionate about
+                educating our next generation of leaders and innovators.
+              </div>
+              <div className='md:text-xl text-lg mt-8 text-[#371064] italic'>
+                This website was created by me from scratch showcasing my digital art skills and web development projects.
+              </div>
             </div>
           </div>
 
@@ -26,20 +28,9 @@ export const Intro = () => {
             <div className='lg:mb-0 mb-2.5'>
               <a className='flex items-center gap-4' href='https://github.com/brar-navleen' target='_blank' rel="noreferrer">
                 <button><img className={`lg:w-9 md:w-9 w-7 rounded-sm ${buttonShadowEffect} shadow-[#00000094] hover:shadow-[#00000094]`} src={githubImage} /></button>
-                Github
+                My software development projects on GitHub
               </a>
             </div>
-
-            <a className='flex items-center gap-4' href='https://www.linkedin.com/in/navleen-brar-5153b725b/' target='_blank' rel="noreferrer">
-              <button><img className={`lg:w-9 md:w-9 w-7 rounded-sm ${buttonShadowEffect} shadow-[#5d92b9] hover:shadow-[#5d92b9]`} src={linkedInImage} /></button>
-              LinkedIn
-            </a>
-
-            <a className='flex items-center gap-4 mt-2.5' href={myResume} target='_blank' rel="noreferrer">
-              <button><img className={`lg:w-9 md:w-9 w-7 rounded-sm ${buttonShadowEffect} shadow-[#5d92b9] hover:shadow-[#5d92b9]`} src={resumeImage} /></button>
-              Resume
-            </a>
-
           </div>
         </div>
         <div className='flex-1'>
