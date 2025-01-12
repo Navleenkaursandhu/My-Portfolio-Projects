608f24c2246cc0a44fa2abc10f3775c9a15449b0
Mantaseus <36828664+Mantaseus@users.noreply.github.com>
2025-01-12 15:35:35 -0800

    Update the portfolio with teaching information (#183)
    * commit the source krita file for the main image on the portfolio page
    
    * Update the portfolio to highlight my teaching experience
    
    * update the `portfolio-image` image to include teaching based icons
    
    * correct some dates

--- src/react/portfolio/Education.tsx
+++ src/react/portfolio/Education.tsx
@@ -6,18 +6,32 @@ export const Education = () => {
       </div>
 
       <div className="mt-8 lg:text-xl md:text-lg text-md grid lg:grid-cols-[auto_minmax(0,1fr)] grid-rows-[auto_minmax(0,1fr)]">
-        <div className="flex lg:border-r lg:border-b-0 border-black lg:pr-6 lg:mr-6 border-b">2018 - 2020</div>
+        <div className="flex lg:border-r lg:border-b-0 border-black lg:pr-6 lg:mr-6 justify-end border-b">Sep 2024 - Jul 2025</div>
+        <div className="mb-4">
+          <div className="font-bold">Bachelors of Education</div>
+          <div>University of British Columbia</div>
+          <div>Vancouver, BC, Canada</div>
+        </div>
+
+        <div className="flex lg:border-r lg:border-b-0 border-black lg:pr-6 lg:mr-6 justify-end border-b">Mar 2023 - Dec 2023</div>
+        <div className="mb-4">
+          <div className="font-bold">Education Assistant Diploma</div>
+          <div>Coastal College of BC</div>
+          <div>Surrey, BC, Canada</div>
+        </div>
+
+        <div className="flex lg:border-r lg:border-b-0 border-black lg:pr-6 lg:mr-6 justify-end border-b">2018 - 2020</div>
         <div className="mb-4">
           <div className="font-bold">Masters of Engineering in Electrical and Computers</div>
           <div>Carleton University</div>
-          <div>Ottawa, Canada</div>
+          <div>Ottawa, Ontario, Canada</div>
         </div>
 
-        <div className="flex lg:border-r lg:border-b-0 border-black lg:mr-6 lg:mt-0 mt-6 border-b">2013 - 2017</div>
+        <div className="flex lg:border-r lg:border-b-0 border-black lg:pr-6 lg:mr-6 justify-end border-b">2013 - 2017</div>
         <div>
           <div className="font-bold">Bachelors of Technology in Electronics and Communication</div>
           <div>Giani Zail Singh College Campus of Engineering and Technology</div>
-          <div>Bathinda, India</div>
+          <div>Bathinda, Punjab, India</div>
         </div>
       </div>
     </div>
--- src/react/portfolio/Skillset.tsx
+++ src/react/portfolio/Skillset.tsx
@@ -1,65 +1,49 @@
 import reactImage from './assets/react.png'
-import viteImage from './assets/vite.png'
 import jsImage from './assets/js.png'
-import htmlImage from './assets/html.png'
-import tailwindImage from './assets/tailwind.png'
-import cssImage from './assets/css.png'
-import gitImage from './assets/git.png'
-import githubImage from './assets/github.png'
-import vscodeImage from './assets/vscode.png'
-import eslintImage from './assets/eslint.png'
-import restapiImage from './assets/restapi.png'
-import vercelImage from './assets/vercel.png'
+import teacherImage from './assets/teacher.png'
+import mathsImage from './assets/mathematics.png'
+import calculusImage from './assets/calculus.png'
+import electricalImage from './assets/lightning-bolt-.png'
+import stemImage from './assets/stem.png'
+import selImage from './assets/sel.png'
 import { SkillDetails } from './SkillDetails'
 
 const skillSetArray = [
   {
-    skill: 'REACT',
+    skill: 'K-12 Teacher Certification (Ongoing)',
+    image: teacherImage
+  },
+  {
+    skill: 'IS Subject Restricted Certificate - STEM',
+    image: teacherImage
+  },
+  {
+    skill: 'Advanced Mathematics',
+    image: mathsImage
+  },
+  {
+    skill: 'Calculus',
+    image: calculusImage
+  },
+  {
+    skill: 'Science',
     image: reactImage
   },
   {
-    skill: 'VITE',
-    image: viteImage
-  },
-  {
-    skill: 'JAVASCRIPT',
+    skill: 'Web/Software Development',
     image: jsImage
   },
   {
-    skill: 'HTML',
-    image: htmlImage
+    skill: 'Engineering',
+    image: electricalImage
   },
   {
-    skill: 'TAILWIND CSS',
-    image: tailwindImage
+    skill: 'ADST',
+    image: stemImage
   },
   {
-    skill: 'CSS',
-    image: cssImage
-  },
-  {
-    skill: 'GIT',
-    image: gitImage
-  },
-  {
-    skill: 'GITHUB',
-    image: githubImage
-  },
-  {
-    skill: 'REST API',
-    image: restapiImage
-  },
-  {
-    skill: 'ESLINT',
-    image: eslintImage
-  },
-  {
-    skill: 'VS CODE',
-    image: vscodeImage
-  },
-  {
-    skill: 'Vercel',
-    image: vercelImage
+    skill: 'SEL',
+    image: selImage
   }
 ]
 export const Skillset = () => {
@@ -67,7 +51,7 @@ export const Skillset = () => {
     <>
       <div className="flex flex-col px-4 text-[#371064] items-center mt-16 bg-indigo-100 pt-8 pb-10 lg:mx-8 md:mx-8 mx-2 rounded-2xl border border-violet-200">
         <div className="font-bold lg:text-3xl md:text-2xl text-xl">
-          SKILL SET
+          SKILLS & CERTIFICATIONS
         </div>
 
         <div className="flex flex-wrap gap-4 mt-8 w-full justify-center font-bold lg:text-xl md:text-lg text-md">
